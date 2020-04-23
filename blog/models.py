0eef67aeeeba1a86f956414c78ac74b755f19871
Andrii <adm.a.danyliuk@loe.lc>
2020-04-23 13:05:01 +0300

    2 commit add kategoriya for post

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    kategoriya = models.ManyToManyField("Kategoriya", help_text="Вибери категорію статті")
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +17,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Kategoriya(models.Model):
+    """
+    Model representing a book genre (e.g. Science Fiction, Non Fiction).
+    """
+    name = models.CharField(max_length=200, help_text="Введи назву категорії (Адміністрування або Програмування і тд.,тп.")
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
